1fcb0dff097421753634466f9fcd89a242658993
Roc <lpf_0403@163.com>
2017-04-24 15:45:33 +0800

    优化

--- LPFSafeDicArr/Swizzling/NSMutableDictionary+Swizzling.m
+++ LPFSafeDicArr/Swizzling/NSMutableDictionary+Swizzling.m
@@ -15,17 +15,55 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         
+        [objc_getClass("__NSPlaceholderDictionary") swizzleSelector:@selector(initWithObjects:forKeys:count:) withSwizzledSelector:@selector(safeInitWithObjects:forKeys:count:)];
         [objc_getClass("__NSDictionaryM") swizzleSelector:@selector(setValue:forKey:) withSwizzledSelector:@selector(safeSetValue:forKey:)];
         [objc_getClass("__NSDictionaryM") swizzleSelector:@selector(setObject:forKey:) withSwizzledSelector:@selector(safeSetObject:forKey:)];
         [objc_getClass("__NSDictionaryM") swizzleSelector:@selector(removeObjectForKey:) withSwizzledSelector:@selector(safeRemoveObjectForKey:)];
         
     });
 }
+
+- (instancetype)safeInitWithObjects:(const id  _Nonnull  __unsafe_unretained *)objects forKeys: (const id  _Nonnull  __unsafe_unretained *)keys count:(NSUInteger)cnt
+{
+    BOOL hasNilObject = NO;
+    for (NSUInteger i = 0; i < cnt; i++) {
+        if (objects[i] == nil || keys[i] == nil) {
+            hasNilObject = YES;
+#if DEBUG
+            // 如果可以对数组中为nil的元素信息打印出来，增加更容    易读懂的日志信息，这对于我们改bug就好定位多了
+            NSString *errorMsg = [NSString     stringWithFormat:@"数组元素不能为nil，其index为: %lu", i];
+            NSAssert(NO, @"%s %@",__FUNCTION__,errorMsg);
+            
+#endif
+        }
+    }
+    
+    // 因为有值为nil的元素，那么我们可以过滤掉值为nil的元素
+    if (hasNilObject) {
+        id __unsafe_unretained newObjects[cnt];
+        id __unsafe_unretained newKeys[cnt];
+        NSUInteger index = 0;
+        NSUInteger keyIndex = 0;
+        for (NSUInteger i = 0; i < cnt; ++i) {
+            if (objects[i] != nil && keys[i] != nil) {
+                newObjects[index++] = objects[i];
+            }
+        }
+        for (NSUInteger i = 0; i < cnt; ++i) {
+            if (keys[i] != nil && objects[i] != nil) {
+                newKeys[keyIndex++] = keys[i];
+            }
+        }
+        return [self safeInitWithObjects:newObjects forKeys:newKeys count:index];
+    }
+    return [self safeInitWithObjects:objects forKeys:keys count:cnt];
+}
+
 - (void)safeSetValue:(id)value forKey:(NSString *)key
 {
-    if (key == nil || value == nil ) {
+    if (key == nil ) {
 #if DEBUG
-        NSAssert(NO, @"%s call -safeSetValue:forKey:, key或vale为nil", __FUNCTION__);
+        NSAssert(NO, @"%s call -safeSetValue:forKey:, key为nil", __FUNCTION__);
 
 #endif
         return;
